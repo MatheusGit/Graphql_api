0a1d633e5c97aa098747c701b0982c2bd07a7811
Matheus Silva Soares <matheus@localhost.localdomain>
2018-03-05 21:17:33 -0300

    Primeiro commit

---
+++ src/graphql/schema.ts
@@ -0,0 +1,46 @@
+import { makeExecutableSchema } from 'graphql-tools';
+
+const users: any[] = [
+    {
+        id: 1, 
+        name: 'Jon',
+        email: 'Jon@email.com'
+    },
+    {
+        id: 2, 
+        name: 'Dayne',
+        email: 'Dany@email.com'
+    }
+]
+
+const typeDefs = `
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+    }
+
+    type Query {
+        allUsers: [User!]!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User
+    }
+
+`;
+
+const resolvers = {
+    Query: {
+        allUsers: () => users
+    },
+    Mutation: {
+        createUser: (parent, args, context, info) => {
+            const newUser = Object.assign({id: users.length+1}, args);
+            users.push(newUser);
+            return newUser;
+        }
+    }
+}
+
+export default makeExecutableSchema({typeDefs, resolvers});
